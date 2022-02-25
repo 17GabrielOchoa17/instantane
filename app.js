e54773723c96327f09a94a6ffcf07ab285918761
Gabriel Ochoa <97166771+17GabrielOchoa17@users.noreply.github.com>
2022-02-25 17:04:00 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// const open = document.getElementById("open");
+// const modal_container = document.getElementById("modal_container");
+// const close = document.getElementById("close");
+
+// open.addEventListener("click", () => {
+//   modal_container.classList.add("show");
+//   alert("prueba");
+// });
+
+// close.addEventListener("click", () => {
+//   modal_container.classList.remove("show");
+// });
+
+const open = document.getElementById("open");
+const modal_container = document.getElementById("modal_container");
+const close = document.getElementById("close");
+
+function OpenImg() {
+  modal_container.classList.add("show");
+  alert("me tocas");
+}
